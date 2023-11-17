8687434edb7124811a6746e35b4f0d69b3e32d9f
kaju111 <saikiakaju275@gmail.com>
2023-11-17 21:03:08 +0530

    backend

--- backend/models/productModels.js
+++ backend/models/productModels.js
@@ -1,82 +1,82 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
-    name:{
+  name: {
+    type: String,
+    required: [true, "Please Enter product Name"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Please Enter product Description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please Enter product price"],
+    maxLength: [8, "Price cann't exceed 8 characters"],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
         type: String,
-        required:[true,"Please Enter product Name"],
-        trim: true
-    },
-    description:{
+        required: true,
+      },
+      url: {
         type: String,
-        required: [true, "Please Enter product Description"]
+        required: true,
+      },
     },
-    price:{
-        type: Number,
-        required: [true, "Please Enter product price"],
-        maxLength:[8, "Price cann't exceed 8 characters"]
-    },
-    ratings:{
-        type: Number,
-        default: 0
-    },
-    images:[
-       {
-        public_id:{
-            type:String,
-            required:true
-        },
-        url:{
-            type:String,
-            required: true
-        }
-       }
-    ],
-    category:{
-        type: String,
-        required: [true,"Please Enter Product Category"],
-    },
-    stock:{
-        type:Number,
-        required:[true, "Please Enter Product Stock"],
-        maxLength:[4,"Stock cann't exceed 4 characters"],
-        default:1
-    },
-    numOfReviews:{
-        type: Number,
-        default:0
-    },
-    reviews:[
-        {
-            user:{
-                type: mongoose.Schema.ObjectId,
-                ref:"User",
-                required:true,
-            },
-            name:{
-                type:String,
-                required:true,
-            },
-            rating:{
-                type:Number,
-                required:true,
-            },
-            comment:{
-                type:String,
-                required: true
-            }
-        }
-    ],
-
-    user:{
-        type:mongoose.Schema.ObjectId,
+  ],
+  category: {
+    type: String,
+    required: [true, "Please Enter Product Category"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please Enter Product Stock"],
+    maxLength: [4, "Stock cann't exceed 4 characters"],
+    default: 1,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      user: {
+        type: mongoose.Schema.ObjectId,
         ref: "User",
         required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
     },
+  ],
 
-    createdAt:{
-        type:Date,
-        default:Date.now
-    } 
-})
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
 
-module.exports = mongoose.model("Product",productSchema)
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
