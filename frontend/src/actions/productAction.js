b2a5210210c51834dc0d7ad6eadb3614d4665553
kaju111 <saikiakaju275@gmail.com>
2023-11-19 20:18:55 +0530

    frontend

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -25,10 +25,13 @@ export const getProduct = () => async (dispatch) => {
   }
 };
 
+// Get Products Details
 export const getProductDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST });
-    const { data } = await axios.get(`/api/v1/products/${id}`);
+
+    const { data } = await axios.get(`/api/v1/product/${id}`);
+
     dispatch({
       type: PRODUCT_DETAILS_SUCCESS,
       payload: data.product,
